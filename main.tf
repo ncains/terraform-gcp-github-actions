b830ca9cb6252bf3bf7929670a6848bc61216fe7
Cains <ncains@outlook.com>
2021-10-13 19:23:51 -0400

    cdn test

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ resource "google_storage_bucket" "bucket" {
   name = "test-bucket-random-001122"
 }
 
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-001123"
+resource "google_compute_backend_bucket" "cdn_backend_bucket" {
+  name        = "cdn-backend-bucket"
+  description = "Backend bucket for serving static content through CDN"
+  bucket_name = google_storage_bucket.bucket.name
+  enable_cdn  = true
 }
